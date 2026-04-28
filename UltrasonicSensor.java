158e3eeb6a26124285989b3d7648ea853555d640
ambashar10-star <ambashar10@gmail.com>
2026-04-29 00:28:05 +0300

    Enhance ultrasonic sensor obstacle avoidance logic
    Refactor ultrasonic sensor logic for improved obstacle avoidance and threading.

--- UltrasonicSensor.java
+++ UltrasonicSensor.java
@@ -59,7 +59,10 @@ class DistanceRunnable implements Runnable {
     }
 }
 
-
+/**
+ * Main class for ultrasonic sensor testing and obstacle avoidance.
+ * Uses threading for sensor reading and performs overtaking logic.
+ */
 public class UltrasonicSensor {
 
     public static void main(String[] args) {
@@ -71,103 +74,197 @@ public class UltrasonicSensor {
 
         // Creating an instance of US sensor at port 2
         EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
-        
-        // Get the distance sample provider
-        SampleProvider distance = ultrasonicSensor.getDistanceMode();
-        
-        // Create a sample array to hold the distance value
-        // even though sonic sensor gives distance as an o/p, but since other sensors, e.g., light sensor
-        // can provide multiple values, therefore to keep consistency, I'm using sampleprovider
-        float[] sample = new float[distance.sampleSize()];
-    
-        // Keep displaying the distance, until user presses a button
-         while (!Button.ESCAPE.isDown())
-         {
-            // Get the curRent distnce reading from the US sensor
-            distance.fetchSample(sample, 0);
+        SampleProvider distanceProvider = ultrasonicSensor.getDistanceMode();
 
-            // // Display the distance on the LCD screen
-            LCD.clear();
-            LCD.drawString("Dist: " + sample[0] + " meters", 0, 0);
+        // Start background thread
+        Thread distThread = new Thread(new DistanceRunnable(distanceProvider));
+        distThread.start();
 
-            // Set speed
-            leftMotor.setSpeed(260);
-            rightMotor.setSpeed(260);
+        leftMotor.setSpeed(200);
+        rightMotor.setSpeed(200);
+
+        // Required for local operations if needed
+        float[] sample = new float[distanceProvider.sampleSize()];
+
+        while (!Button.ESCAPE.isDown()) {
+
+            float currentDist;
+
+            // Simple synchronization (lock) when reading shared data
+            synchronized (ShareData.class) {
+                currentDist = ShareData.distance;
+            }
 
             leftMotor.forward();
             rightMotor.forward();
-            // //Delay.msDelay(3000); 
 
-            if(sample[0] < 0.3){
+            //OBJECT DETECTED
+            if (currentDist < 0.3 && currentDist > 0) {
 
-                LCD.clear();
-                LCD.drawString("Dist: " + sample[0] + " meters", 0, 0);
-
+                leftMotor.stop(true);
+                rightMotor.stop();
                 Sound.beep();
 
-                // Slow down
-                leftMotor.setSpeed(120);
-                rightMotor.setSpeed(120);
-                leftMotor.forward();
+                //SCAN LEFT
+                rightMotor.resetTachoCount();
+                leftMotor.backward();
                 rightMotor.forward();
-                Delay.msDelay(300);
 
-                // slightly turn to the right
-                leftMotor.setSpeed(170);
-                rightMotor.setSpeed(100);
-                leftMotor.forward();
-                rightMotor.forward();
-                Delay.msDelay(3000);
+                while (true) {
+                    synchronized (ShareData.class) {
+                        if (ShareData.distance > 0.6) break;
+                    }
+                }
 
-                //  MOVE BESIDE OBJECT WHILE MAINTAINING LEFT
-                leftMotor.setSpeed(100);
-                rightMotor.setSpeed(150);
-                leftMotor.forward();
-                rightMotor.forward();
-                Delay.msDelay(3000);
+                leftMotor.stop(true);
+                rightMotor.stop();
+                int leftAngle = rightMotor.getTachoCount();
 
-                // move forward again
-                leftMotor.setSpeed(260);
-                rightMotor.setSpeed(260);
-                leftMotor.forward();
-                rightMotor.forward();
-                Delay.msDelay(700);
+                // Return to center
+                leftMotor.rotate(leftAngle, true);
+                rightMotor.rotate(-leftAngle);
 
-                // slightly turn to the left again
-                leftMotor.setSpeed(100);
-                rightMotor.setSpeed(170);
+                //SCAN RIGHT
+                leftMotor.resetTachoCount();
                 leftMotor.forward();
-                rightMotor.forward();
-                Delay.msDelay(5000);
+                rightMotor.backward();
 
-                // slightly turn to the left again (to move forward in a straight line)
-                leftMotor.setSpeed(200);
-                rightMotor.setSpeed(100);
-                leftMotor.forward();
-                rightMotor.forward();
-                Delay.msDelay(3000);
+                while (true) {
+                    synchronized (ShareData.class) {
+                        if (ShareData.distance > 0.6) break;
+                    }
+                }
 
-                // Move forward
-                leftMotor.setSpeed(260);
-                rightMotor.setSpeed(260);
-                leftMotor.forward();
-                rightMotor.forward();
-                Delay.msDelay(3000);
-                break;
+                leftMotor.stop(true);
+                rightMotor.stop();
+                int rightAngle = leftMotor.getTachoCount();
+
+                // Return to center
+                leftMotor.rotate(-rightAngle, true);
+                rightMotor.rotate(rightAngle);
+
+                // DECISION
+                if (leftAngle < rightAngle) {
+
+                    LCD.drawString("Left is Clearer", 0, 2);
+
+                    leftMotor.rotate(-leftAngle, true);
+                    rightMotor.rotate(leftAngle);
+
+                    leftMotor.forward();
+                    rightMotor.forward();
+                    Delay.msDelay(3070);
+
+                    leftMotor.rotate(leftAngle, true);
+                    rightMotor.rotate(-leftAngle);
+
+                    boolean passed = false;
+
+                    while (!passed) {
+
+                        leftMotor.forward();
+                        rightMotor.forward();
+                        Delay.msDelay(1000);
+
+                        leftMotor.stop(true);
+                        rightMotor.stop();
+
+                        // Peek right
+                        leftMotor.rotate(180, true);
+                        rightMotor.rotate(-180);
+
+                        synchronized (ShareData.class) {
+                            if (ShareData.distance < 0.5) {
+                                leftMotor.rotate(-180, true);
+                                rightMotor.rotate(180);
+                            } else {
+                                leftMotor.rotate(-180, true);
+                                rightMotor.rotate(180);
+
+                                leftMotor.forward();
+                                rightMotor.forward();
+                                Delay.msDelay(1000);
+
+                                leftMotor.rotate(leftAngle, true);
+                                rightMotor.rotate(-leftAngle);
+
+                                leftMotor.forward();
+                                rightMotor.forward();
+                                Delay.msDelay(3070);
+
+                                leftMotor.rotate(-leftAngle, true);
+                                rightMotor.rotate(leftAngle);
+
+                                passed = true;
+                            }
+                        }
+                    }
+                } 
+                else {
+
+                    LCD.drawString("Right is Clearer", 0, 2);
+
+                    leftMotor.rotate(rightAngle, true);
+                    rightMotor.rotate(-rightAngle);
+
+                    leftMotor.forward();
+                    rightMotor.forward();
+                    Delay.msDelay(3070);
+
+                    leftMotor.rotate(-rightAngle, true);
+                    rightMotor.rotate(rightAngle);
+
+                    boolean passed = false;
+
+                    while (!passed) {
+
+                        leftMotor.forward();
+                        rightMotor.forward();
+                        Delay.msDelay(1000);
+
+                        leftMotor.stop(true);
+                        rightMotor.stop();
+
+                        // Peek left
+                        leftMotor.rotate(-180, true);
+                        rightMotor.rotate(180);
+
+                        synchronized (ShareData.class) {
+                            if (ShareData.distance < 0.5) {
+                                leftMotor.rotate(180, true);
+                                rightMotor.rotate(-180);
+                            } else {
+                                leftMotor.rotate(180, true);
+                                rightMotor.rotate(-180);
+
+                                leftMotor.forward();
+                                rightMotor.forward();
+                                Delay.msDelay(1000);
+
+                                leftMotor.rotate(-rightAngle, true);
+                                rightMotor.rotate(rightAngle);
+
+                                leftMotor.forward();
+                                rightMotor.forward();
+                                Delay.msDelay(3070);
+
+                                leftMotor.rotate(rightAngle, true);
+                                rightMotor.rotate(-rightAngle);
+
+                                passed = true;
+                            }
+                        }
+                    }
+                }
             }
-            
-            // Refresh display every 100 ms
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        
-         }
+        }
 
-        // leftMotor.stop(true);
-        // rightMotor.stop(true);
+        //CLEANUP 
+        ShareData.running = false;
 
+        leftMotor.close();
+        rightMotor.close();
+        ultrasonicSensor.close();
     }
 }
 
