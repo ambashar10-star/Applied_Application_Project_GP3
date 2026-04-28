d2b81d693043bb68f38afe18326bb65318d6e9c3
ambashar10-star <ambashar10@gmail.com>
2026-04-29 00:44:24 +0300

    Added JavaDoc documentation

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"src","c":"FinalWork","l":"FinalWork()","u":"%3Cinit%3E()","k":"3"},{"p":"src","c":"FinalWork","l":"main(String[])","u":"main(java.lang.String[])","k":"6"}];updateSearchResults();
